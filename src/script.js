2a74c8ca36e531a1a5b1824a17a8ca63eb6976ad
Jessie Lonsdale <lonsdale.jess@gmail.com>
2023-11-23 13:45:30 +0100

    save point

--- src/script.js
+++ src/script.js
@@ -2,9 +2,9 @@ window.onload = function () {
   main();
 };
 
-let scrollValue = 1;
-const minscrollValue = 1;
-const maxscrollValue = 10;
+let scrollValue = 3;
+const minscrollValue = 3;
+const maxscrollValue = 100;
 
 async function main() {
   // Event Listeners
@@ -23,6 +23,14 @@ async function main() {
   const adapter = await gpu.requestAdapter();
   const device = await adapter.requestDevice();
   const canvas = document.getElementById("c");
+
+  let response = await fetch("../assets/Diamond_Low_Poly.obj");
+  let objtext = await response.text();
+  let objects = await readObj(objtext);
+
+  vertices = new Float32Array(objects[0].v);
+  normals = new Float32Array(objects[0].n);
+  indices = new Int32Array(objects[0].f);
 
   const context =
     canvas.getContext("gpupresent") || canvas.getContext("webgpu");
@@ -51,24 +59,64 @@ async function main() {
     },
   });
 
-  const uniformBuffer = device.createBuffer({
-    size: 35, // number of bytes
+  const vertexBuffer = await device.createBuffer({
+    size: (vertices.byteLength + 3) & ~3,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  });
+  const indexBuffer = await device.createBuffer({
+    size: (indices.byteLength + 3) & ~3,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  });
+  const normalBuffer = await device.createBuffer({
+    size: (normals.byteLength + 3) & ~3,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  });
+
+  let funiforms = new Float32Array([scrollValue]);
+  let iuniforms = new Uint32Array([(numberOfTriangles = vertices.length / 4)]);
+
+  await device.queue.writeBuffer(vertexBuffer, 0, vertices);
+  await device.queue.writeBuffer(indexBuffer, 0, indices);
+  await device.queue.writeBuffer(normalBuffer, 0, normals);
+
+  const f_uniformBuffer = await device.createBuffer({
+    size: funiforms.byteLength,
     usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
   });
+
+  const i_uniformBuffer = await device.createBuffer({
+    size: iuniforms.byteLength,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+  });
+
+  await device.queue.writeBuffer(f_uniformBuffer, 0, funiforms);
+  await device.queue.writeBuffer(i_uniformBuffer, 0, iuniforms);
 
   const bindGroup = device.createBindGroup({
     layout: pipeline.getBindGroupLayout(0),
     entries: [
-      {
-        binding: 0,
-        resource: { buffer: uniformBuffer },
-      },
+      { binding: 0, resource: { buffer: f_uniformBuffer } },
+      { binding: 1, resource: { buffer: i_uniformBuffer } },
+      { binding: 2, resource: { buffer: vertexBuffer } },
+      { binding: 3, resource: { buffer: indexBuffer } },
+      { binding: 4, resource: { buffer: normalBuffer } },
     ],
   });
 
   const render = () => {
-    let uniforms = new Float32Array([scrollValue]);
-    device.queue.writeBuffer(uniformBuffer, 0, uniforms);
+    let funiforms = new Float32Array([scrollValue]);
+    device.queue.writeBuffer(f_uniformBuffer, 0, funiforms);
+
+    const bindGroup = device.createBindGroup({
+      layout: pipeline.getBindGroupLayout(0),
+      entries: [
+        { binding: 0, resource: { buffer: f_uniformBuffer } },
+        { binding: 1, resource: { buffer: i_uniformBuffer } },
+        { binding: 2, resource: { buffer: vertexBuffer } },
+        { binding: 3, resource: { buffer: indexBuffer } },
+        { binding: 4, resource: { buffer: normalBuffer } },
+      ],
+    });
 
     const encoder = device.createCommandEncoder();
     const pass = encoder.beginRenderPass({
